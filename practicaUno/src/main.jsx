6b47a7178a386cf09557e5158760716997784f13
Gonzalo <gonzalo-r@hotmail.es>
2024-08-12 23:18:58 -0300

    a

--- practicaUno/src/main.jsx
+++ practicaUno/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import ModuloGon1 from "./ModuloGon1.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <ModuloGon1 />
+  </StrictMode>
+);

8b218a32454f1c6d68e2dae5bee81170deed9d76
Gonzalo <gonzalo-r@hotmail.es>
2024-08-14 22:12:33 -0300

    a

--- practicaUno/src/App.jsx
+++ practicaUno/src/App.jsx
@@ -1,10 +1,28 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "./components/Navbar/Navbar";
+import { useState } from "react";
+import { Button, Text, Flex } from "@chakra-ui/react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleRemoveItem = () => {
+    setCount(count - 1);
+  };
+
+  const handleAddItem = () => {
+    setCount(count + 1);
+  };
+
+  console.log(count);
   return (
     <ChakraProvider>
       <Navbar></Navbar>
+      <Flex>
+        <Button onClick={handleAddItem}>+</Button>
+        <Text>{count}</Text>
+        <Button onClick={handleRemoveItem}>-</Button>
+      </Flex>
     </ChakraProvider>
   );
 }

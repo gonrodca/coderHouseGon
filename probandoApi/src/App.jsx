8fb617d3757de9ce2ed235dfa7fe4866376a49cd
Gonzalo <gonzalo-r@hotmail.es>
2024-08-27 21:31:02 -0300

    a

--- probandoApi/src/App.jsx
+++ probandoApi/src/App.jsx
@@ -1,5 +1,15 @@
+import * as React from "react";
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 function App() {
-  return <></>;
+  // 2. Wrap ChakraProvider at the root of your app
+  return (
+    <ChakraProvider>
+      <NavBar></NavBar>
+    </ChakraProvider>
+  );
 }
 
 export default App;

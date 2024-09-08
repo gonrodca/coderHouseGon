73cfcfb371537bb1b6d98e28ad381bc4dc86a635
Gonzalo Rodriguez <gonchi@192.168.1.4>
2024-09-08 16:38:38 -0300

    a

--- SegundaPreEntregaRodriguez/src/router/MainRouter.jsx
+++ SegundaPreEntregaRodriguez/src/router/MainRouter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+//https://coderhouse.zoom.us/rec/play/NYVjKWk2vUDpQozh7JoLXaUCkZO1aWbhJd7qbXL2PHw7hpu3VUggojX1_XzrPZu6YDJhN_wABUK3yg9u.X02AuyOgrO5_H4i2?canPlayFromShare=true&from=share_recording_detail&continueMode=true&componentName=rec-play&originRequestUrl=https%3A%2F%2Fcoderhouse.zoom.us%2Frec%2Fshare%2FksdYEMoeIxB_1Lpjj18ZhxPgPFqRf_WN3dv3D5Jyghzn6fWy99Iy6GAQG_n0yGSO.0bKRREh5QbhOCyuz
 export const MainRouter = () => {
   return <div>MainRouter</div>;
 };

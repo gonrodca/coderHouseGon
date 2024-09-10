66dcaf3070770f79ca4f9f097e50df97f02ef019
Gonzalo Rodriguez <gonchi@192.168.1.4>
2024-09-09 21:18:18 -0300

    SegundaEntregaRodriguez

--- SegundaPreEntregaRodriguez/src/hooks/useCategoryId.jsx
+++ SegundaPreEntregaRodriguez/src/hooks/useCategoryId.jsx
@@ -2,13 +2,12 @@ import { useEffect, useState } from "react";
 import { categoryProduct } from "../services/products";
 
 export const useCategoryId = ({ id }) => {
-  const [dataProducts, setDataProducts] = useState({});
+  const [dataProducts, setDataProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   //setDataProducts(resp.products)
-  console.log(id);
   useEffect(() => {
-    categoryProduct(id)
-      .then((resp) => setDataProducts(resp))
+    categoryProduct({ id })
+      .then((resp) => setDataProducts(resp.products))
       .catch(console.error)
       .finally(setLoading(false));
   }, [id]);
--- SegundaPreEntregaRodriguez/src/pages/Category.jsx
+++ SegundaPreEntregaRodriguez/src/pages/Category.jsx
@@ -1,12 +1,12 @@
-import React from "react";
 import { useParams } from "react-router";
 import { ItemListContainer } from "../components";
 import { useCategoryId } from "../hooks";
-import { data } from "framer-motion/client";
 
 export const Category = () => {
   const { id } = useParams();
+
   const { dataProducts, loading } = useCategoryId({ id });
-  console.log(dataProducts);
+
+  console.log({ dataProducts });
   return <ItemListContainer products={dataProducts}></ItemListContainer>;
 };
